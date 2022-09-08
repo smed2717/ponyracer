7e14341dbf41556a9397b17f604c2ff0c4673716
MOHAMED SMAÏRI <smairimohamed@gmail.com>
2022-09-08 23:18:30 +0100

    race list setup

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  navbarCollapsed = true;
+  toggleNavbar() {
+    this.navbarCollapsed = !this.navbarCollapsed;
+  }
 
   constructor() { }
 
   ngOnInit() {
+
   }
+
 
 }
